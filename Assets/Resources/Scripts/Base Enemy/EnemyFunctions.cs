489905b72496d8dee02ab1cc34100b274d9a238d
TheBestestGuestest <ryanniu314@gmail.com>
2019-09-20 21:07:06 -0700

    moving files around

---
+++ Assets/Resources/Scripts/Base Enemy/EnemyFunctions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFunctions {
+    protected Transform trans;
+    protected Func<float, Vector3> temp;
+    protected Equation eq;
+    protected List<Function> funcList;
+
+    public EnemyFunctions(Transform transform) {
+        trans = transform;
+        funcList = new List<Function>();
+        cacheAll();
+    }
+    protected virtual void cacheAll() { }
+    protected void setFunctionActiveState(int index, bool active) {
+        Function func = getFunction(index);
+        if (active) {
+            if (func.gameObject.activeInHierarchy) throw new Exception("Function is already active!");
+            else func.gameObject.SetActive(true);
+        }
+        else {
+            if (!func.gameObject.activeInHierarchy) throw new Exception("Function is already inactive!");
+            else func.disableFunction();
+        }
+    }
+    public void setAllActiveState(bool active) {
+        for (int i = 0; i < funcList.Count; i++) setFunctionActiveState(i, active);
+    }
+    public Function getFunction(int index) {
+        if (index >= funcList.Count || index < 0) throw new Exception("Function index out of range! (" + index + ")");
+        return funcList[index];
+    }
+    public int getCount() {
+        return funcList.Count;
+    }
+    public bool allFunctionsIdle() {
+        foreach (Function func in funcList) if (func.getCurrProcess() != FunctionProcess.IDLE) return false;
+        return true;
+    }
+}
---
+++ Assets/Resources/Scripts/Graph/InGameDimentions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class InGameDimentions {
+    public static float rightEdge = 3f;
+    public static float leftEdge = -20.0f / 3.0f;
+    public static float topEdge = 5f;
+    public static float bottomEdge = -5f;
+
+    public static float screenWidth = rightEdge - leftEdge;
+    public static float screenHeight = topEdge - bottomEdge;
+
+    public static float centerX = (rightEdge + leftEdge) / 2.0f;
+    public static float centerY = (topEdge + bottomEdge) / 2.0f;
+
+    public static Vector3 center = new Vector3(centerX, centerY, 0);
+}
